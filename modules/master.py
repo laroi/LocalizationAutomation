ddf2d9758d8c7436764a91dce3cb6672775f0be4
laroi <akbarali1klr@gmail.com>
2014-09-20 17:31:33 +0530

    started working on master.py to update master files

--- modules/master.py
+++ modules/master.py
@@ -18,22 +18,40 @@ import utils
 #########################################################################################################################
 
 _envData = utils.getEnvData()
-jsonData = ""
-def updateMasterFile(variable, section, newSection, description, platforms, languages, tag):
-    if(!variable):
+jsonData = {}
+locale = {}
+platform = {}
+def updateMasterFile(variable, section, newSection, description, tag, platforms, languages):
+    if(not variable):
         print "Variable not found"
-        return false;
-    if(!section):
+        return False;
+    if(not section):
         print "section not found"
-        return false;
-    if(!tag):
+        return False;
+    if(not tag):
         print "tag not found"
-        return false;
+        return False;
 
     masterData = utils.readJson('../res/master.json')
     if (newSection):
-        
-    
+        jsonData[section] = {variable:{'locale':{}, 'platform':{}}}
+    else:
+        if(masterData[section]):
+            jsonData[section] = {variable:{'locale':{}, 'platform':{}}}
+    if(languages and len(languages) > 0):
+        for lan in laguages:
+            locale [lan] = ""
+    else:
+        for lan in _envData["target"]:
+            locale [lan] = ""
+    if(platforms and len(platforms) > 0):
+        for plat in platforms:
+            platform [plat] = True
+    else:
+        for plat in _envData["platform"]:
+            platform [plat] = True
+    jsonData[section][variable]['locale'] = locale
+    jsonData[section][variable]['platform'] = platform
+    print jsonData
 
-updateMasterFile()
-
+updateMasterFile("m_login", "log in", True, "this is test", "login", False, False);
